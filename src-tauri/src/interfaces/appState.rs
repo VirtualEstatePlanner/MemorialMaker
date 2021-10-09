28e3e77bfaf9e5e2739bc876e59cc166cc8d19d2
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-10-09 13:09:03 -0400

    basic struct for AppState

---
+++ src-tauri/src/interfaces/appState.rs
@@ -0,0 +1,12 @@
+pub(crate) mod IAppState;
+pub(crate) mod IEmailAddress;
+pub(crate) mod IContactPerson;
+pub(crate) mod IMemoriliazedPerson;
+pub(crate) mod IName;
+pub(crate) mod IPerson;
+pub(crate) mod IServiceAgendaItem;
+
+pub fn new() {
+    struct IAppState {
+    }
+}
